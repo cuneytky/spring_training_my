69e1646db18479ce364b1af1892cafdec21f7b0f
cuneyt <cuneytky@gmail.com>
2023-09-13 11:06:28 +0300

    spring-12-Table relatinship start

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -0,0 +1,23 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "DATE")
+    private LocalDate createdDate;
+
+    private BigDecimal amount;
+
+}
