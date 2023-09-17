6421d91d08198f9b652e40846633f766a39020c3
cuneyt <cuneytky@gmail.com>
2023-09-17 18:24:17 +0300

    spring-12-Table added relationship

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -1,5 +1,6 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Status;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +18,21 @@ public class Payment {
     private Long id;
     @Column(columnDefinition = "DATE")
     private LocalDate createdDate;
-
     private BigDecimal amount;
+    @Enumerated(EnumType.STRING)
+    private Status paymentStatus;
 
+    //@OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    @JoinColumn(name = "payment_detail_id")
+    private PaymentDetail paymentDetail;
+
+    @ManyToOne
+    private Merchant merchant;
+
+    public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
+        this.createdDate = createdDate;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
 }
